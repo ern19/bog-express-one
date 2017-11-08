35a09900a92cc8c7a4d5737f91bcf7633be9ec97
ern19 <aarontrammell19@gmail.com>
2017-11-08 09:49:31 -0500

    pushin stuff

---
+++ routes/creatureController.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const router = express.Router()
+
+const { Creature } = require('../db/schema.js')
+
+router.get('/', async (req,res) => {
+    try {
+        const creatures = await Creature.find({})
+        res.json(creatures)
+    } catch(err) {
+        console.log(err)
+    }
+})
+
+router.get('/:id', async (req,res) => {
+    try {
+        const creatureId = req.params.id
+        const creature = await Creature.findById(creatureId)
+        res.json(creature)
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
+router.post('/', async (req,res) => {
+    try {
+        const newCreature = req.body
+        const savedCreature = await Creature.create(newCreature)
+        res.json(savedCreature)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+router.patch('/:id', async (req,res) => {
+    try {
+        const creatureId = req.params.id
+        const updatedCreature = req.body
+        const savedCreature = await Creature.findByIdAndUpdate(creatureId, updatedCreature)
+        res.json(savedCreature)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    try {
+        const creatureId = req.params.id
+        await Creature.findByIdAndRemove(creatureId)
+        res.json({
+            msg: 'successfully deleted'
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+module.exports = router;
